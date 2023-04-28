18e002e10c619bab247f8e31ba3898d3044b7b88
Mário Santos <mariodev7@gmail.com>
2023-04-28 17:41:10 -0300

    fix(next-config): fix module exports

--- next.config.js
+++ next.config.js
@@ -1,22 +1,12 @@
-const { i18n } = require('./next-i18next.config')
+const { i18n } = require("./next-i18next.config");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        destination: "/404",
-        permanent: false,
-      },
-    ];
-  },
   reactStrictMode: true,
   i18n: {
     localeDetection: true,
-    locales: ['en', 'pt']
-  }
-}
+    locales: ["en", "pt"],
+  },
+};
 
-
-module.exports = { nextConfig }
+module.exports = nextConfig;
